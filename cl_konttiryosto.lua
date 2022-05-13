a9a2831031c7c5cbf21f124dd1e69eba3fbf257e
TonttuTJ <68849401+JonttuJT@users.noreply.github.com>
2022-05-13 20:13:24 +0300

    optimoinita ja näin

--- cl_konttiryosto.lua
+++ cl_konttiryosto.lua
@@ -39,7 +39,7 @@ Citizen.CreateThread(function()
         end, i)
     end
     while true do
-        Wait(0)
+        local sleep = 2000
         local player = PlayerPedId()
         local coords = GetEntityCoords(player)
         for i = 1, #Config.Kontit do
@@ -47,6 +47,7 @@ Citizen.CreateThread(function()
             local d = v.Ovi
             local door = GetClosestObjectOfType(d.Koordinaatit, 2.0, GetHashKey(d.Objekti), false, 0, 0)
             local dist = #(coords - d.Koordinaatit)
+
             if door then
                 FreezeEntityPosition(door, d.Kiinni)
                 if d.Kiinni then
@@ -54,65 +55,66 @@ Citizen.CreateThread(function()
                 end
             end
             if dist <= 10.0 then
-                if dist <= 2.0 then
-                    if d.Kiinni and ESX.PlayerData.job.name ~= 'police' then
-                        if tiirikka or GetSelectedPedWeapon(player) == GetHashKey("WEAPON_CROWBAR") then
-                            ESX.ShowHelpNotification('Paina ~INPUT_CONTEXT~ murtaaksesi oven')
-                            if IsControlPressed(0, 38) then
-                                TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_BUM_BIN", 0, true)
-					            Citizen.Wait(1000)
-					            if IsPedUsingAnyScenario(PlayerPedId()) == false then
-						            TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_BUM_BIN", 0, true)
-					            end
-                                Citizen.Wait(1000)
-					            if IsPedUsingAnyScenario(PlayerPedId()) then
-                                    ClearPedTasks(PlayerPedId())
-                                    ESX.ShowNotification('~g~Onnistuit murrossa!')
-                                    TriggerServerEvent('esx_konttiryosto:setDoorFreezeStatus', i, false)
-                                    TriggerServerEvent('esx_addons_gcphone:startCall', 'police', 'Konttimurto', coords, {
-                                        coords = { x = d.Koordinaatit.x, y = d.Koordinaatit.y, z = d.Koordinaatit.z },
-                                    })
-                                else 
-                                    ESX.ShowNotification('~r~Epäonnistuit murrossa!')
-                                end
-                            end
-                        else
-                            Wait(1000)
-                        end
-                    elseif not d.Kiinni and ESX.PlayerData.job.name == 'police' then
-                        ESX.ShowHelpNotification('Paina ~INPUT_CONTEXT~ korjataksesi oven')
+                sleep = 5
+            end
+            if dist <= 2.0 then
+                if d.Kiinni and ESX.PlayerData.job.name ~= 'police' then
+                    if tiirikka or GetSelectedPedWeapon(player) == GetHashKey("WEAPON_CROWBAR") then
+                        ESX.ShowHelpNotification('Paina ~INPUT_CONTEXT~ murtaaksesi oven')
                         if IsControlPressed(0, 38) then
                             TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_BUM_BIN", 0, true)
-					        Citizen.Wait(15000)
+					        Citizen.Wait(1000)
 					        if IsPedUsingAnyScenario(PlayerPedId()) == false then
 						        TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_BUM_BIN", 0, true)
 					        end
-                            Citizen.Wait(15000)
+                            Citizen.Wait(1000)
 					        if IsPedUsingAnyScenario(PlayerPedId()) then
                                 ClearPedTasks(PlayerPedId())
-                                ESX.ShowNotification('~g~Onnistuit!')
-                                TriggerServerEvent('esx_konttiryosto:setDoorFreezeStatus', i, true)
+                                ESX.ShowNotification('~g~Onnistuit murrossa!')
+                                TriggerServerEvent('esx_konttiryosto:setDoorFreezeStatus', i, false)
+                                TriggerServerEvent('esx_addons_gcphone:startCall', 'police', 'Konttimurto', coords, {
+                                    coords = { x = d.Koordinaatit.x, y = d.Koordinaatit.y, z = d.Koordinaatit.z },
+                                })
+                            else 
+                                ESX.ShowNotification('~r~Epäonnistuit murrossa!')
                             end
                         end
                     else
                         Wait(1000)
                     end
+                elseif not d.Kiinni and ESX.PlayerData.job.name == 'police' then
+                    ESX.ShowHelpNotification('Paina ~INPUT_CONTEXT~ korjataksesi oven')
+                    if IsControlPressed(0, 38) then
+                        TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_BUM_BIN", 0, true)
+					    Citizen.Wait(15000)
+					    if IsPedUsingAnyScenario(PlayerPedId()) == false then
+						    TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_BUM_BIN", 0, true)
+					    end
+                        Citizen.Wait(15000)
+					    if IsPedUsingAnyScenario(PlayerPedId()) then
+                            ClearPedTasks(PlayerPedId())
+                            ESX.ShowNotification('~g~Onnistuit!')
+                            TriggerServerEvent('esx_konttiryosto:setDoorFreezeStatus', i, true)
+                        end
+                    end
                 end
-            else 
-                Wait(1000)
             end
         end
+        Wait(sleep)
     end
 end)
 
 Citizen.CreateThread(function()
     while true do
-        Wait(0)
+        local sleep = 2000
         for k, v in pairs(Config.Kontit) do
             for i = 1, #v.Kohdat do
                 local player = PlayerPedId()
                 local coords = GetEntityCoords(player)
                 local dist = #(coords - v.Kohdat[i].Paikka)
+                if dist <= 10 then
+                    sleep = 5
+                end
                 if not v.Ovi.Kiinni then
                     if dist <= 0.5 then
                         if not v.Kohdat[i].Otettu then
@@ -137,7 +139,8 @@ Citizen.CreateThread(function()
                     Wait(1000)
                 end
             end
-        end 
+        end
+        Wait(sleep)
     end
 end)
 
